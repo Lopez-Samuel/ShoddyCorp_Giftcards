3863fe3b8f8534d5f55d8a240eb8af671dcc96f6
rangelo313 <rac16c@my.fsu.edu>
2021-11-06 12:48:30 -0700

    message

--- part3/views.py
+++ part3/views.py
@@ -15,16 +15,18 @@ SALT_LEN = 16
 
 # Prometheus stuff!
 graphs = {}
-graphs['r_counter'] = Counter('python_request_r_posts', 'The total number'\
-  + ' of register posts.')
-graphs['l_counter'] = Counter('python_request_l_posts', 'The total number'\
-  + ' of login posts.')
+#graphs['r_counter'] = Counter('python_request_r_posts', 'The total number'\
+#  + ' of register posts.')
+#graphs['l_counter'] = Counter('python_request_l_posts', 'The total number'\
+#  + ' of login posts.')
 graphs['b_counter'] = Counter('python_request_b_posts', 'The total number'\
   + ' of card buy posts.')
 graphs['g_counter'] = Counter('python_request_g_posts', 'The total number'\
   + ' of card gift posts.')
-graphs['u_counter'] = Counter('python_request_u_posts', 'The total number'\
-  + ' of card use posts.')
+#graphs['u_counter'] = Counter('python_request_u_posts', 'The total number'\
+ # + ' of card use posts.')
+graphs['err_counter'] = Counter('python_404', 'The total number'\
+  + ' of 404s errors.')
 
 # Create your views here.
 # Landing page. Nav bar, most recently bought cards, etc.
@@ -98,11 +100,13 @@ def buy_card_view(request, prod_num=0):
             try:
                 prod = Product.objects.get(product_id=prod_num) 
             except:
+                graphs['err_counter'].inc()
                 return HttpResponse("ERROR: 404 Not Found.")
         else:
             try:
                 prod = Product.objects.get(product_id=1) 
             except:
+                graphs['err_counter'].inc()
                 return HttpResponse("ERROR: 404 Not Found.")
         context['prod_name'] = prod.product_name
         context['prod_path'] = prod.product_image_path
@@ -148,11 +152,13 @@ def gift_card_view(request, prod_num=0):
             try:
                 prod = Product.objects.get(product_id=prod_num) 
             except:
+                 graphs['err_counter'].inc()
                 return HttpResponse("ERROR: 404 Not Found.")
         else:
             try:
                 prod = Product.objects.get(product_id=1) 
             except:
+                 graphs['err_counter'].inc()
                 return HttpResponse("ERROR: 404 Not Found.")
         context['prod_name'] = prod.product_name
         context['prod_path'] = prod.product_image_path
@@ -165,6 +171,7 @@ def gift_card_view(request, prod_num=0):
             prod_num = 1
         user = request.POST.get('username', None)
         if user is None:
+            graphs['err_counter'].inc()
             return HttpResponse("ERROR 404")
         try:
             user_account = User.objects.get(username=user)
@@ -250,6 +257,7 @@ def use_card_view(request):
             user_cards = None
         context['card_list'] = user_cards
         return render(request, "use-card.html", context)
+    graphs['err_counter'].inc()
     return HttpResponse("Error 404: Internal Server Error")
 
 def metrics_view(request):
